a07f67fdfbdf278ae7d44fc6f29540aff5c2c094
MavithaShehar <mavithashehar@gmail.com>
2024-08-03 00:00:51 +0530

    Rest Operator with  arry

--- src/pages/RestOperator.jsx
+++ src/pages/RestOperator.jsx
@@ -5,6 +5,9 @@ import HeaderContent from "../componets/HeaderContend/HeaderContent"
 function RestOperator() {
 
 // Rest Operator    
+
+// Exampel 01
+
 let total =0; 
 function sume(...sumnumbers){
     sumnumbers.forEach(sumnumber => {
@@ -15,6 +18,13 @@ function sume(...sumnumbers){
 }
 
 console.log(sume(1,3,5,5,6,3,3))
+
+// Example 02
+
+function myBio(firstName,SecondName,...rest){
+console.log(firstName,SecondName,rest)
+}
+myBio("Shehar","Mavitha","A","B","C" );
 
   return (
     <>
